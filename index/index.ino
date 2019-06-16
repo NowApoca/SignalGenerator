04ccf9ca3d1c3948d16e0bb6b9dcd5cfe2bbef23
Pukitala <manueldiez123@gmail.com>
2019-06-16 19:28:22 -0300

    Triangular and Sine working

--- index/index.ino
+++ index/index.ino
@@ -4,15 +4,15 @@ const int DACresolution = 256;
 volatile int interruptCounter;
 bool firstOn = true;
 bool sineOn = false;
-float sineValues[DACresolution];
-float triangularValues[DACresolution];
+int sineValues[DACresolution];
+int triangularValues[DACresolution];
 int busFq = 80000000;
 
 hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
  
 void IRAM_ATTR settedUpFunctionsTimer(){
-  static float actualValueCount = 0; 
+  static int actualValueCount = 0; 
   portENTER_CRITICAL_ISR(&timerMux);
   if(firstOn){
     actualValueCount = 0;
@@ -43,18 +43,25 @@ void IRAM_ATTR pulseTimer() {
 
 void setup() {
   Serial.begin(115200);
-  initializeSineArray()
+  initializeSineArray();
+  initializeTriangularArray(1000,50);
 }
 
 void loop() { // Generate a Sine wave
-  // if(firstOn){
-  //   sineOn(1000);
-  // }
+  if(firstOn){
+    firstOn = false;
+    Serial.print(2);
+    int a = 0;
+    for(;a<DACresolution;a++){
+      Serial.print("\n triangularValues Value: ");
+      Serial.print(triangularValues[a]);
+    }
+  }
 }
 
 void initializeTimer(int fq){
   timer = timerBegin(0, int(busFq/fq), true);
-  timerAttachInterrupt(timer, &onTimer, true);
+  timerAttachInterrupt(timer, &settedUpFunctionsTimer, true);
   timerAlarmWrite(timer, 1, true);
   timerAlarmEnable(timer);
 }
@@ -63,7 +70,7 @@ float degToRad(float deg){
   return deg*100000 / 57296;
 }
 
-int sineOn(float deg){
+int sineGeneratorOn(float deg){
   return deg*100000 / 57296;
 }
 
@@ -77,16 +84,14 @@ int triangularOn(float deg){
 
 void initializeTriangularArray(int frequency, float trise){
   float period = 1/(float(frequency));
-  int inflexionPoint = DACresolution*(trise/period)
+  int inflexionPoint = (DACresolution*trise)/100;
   int actPosition = 0;
-  int valueTriangular = ;
-  for(; actPosition > DACresolution ; actPosition++){
+  for(; actPosition < DACresolution ; actPosition++){
     if(actPosition < inflexionPoint){
-      triangularValues[actPosition] = int((actPosition/inflexionPoint)*DACresolution);
+      triangularValues[actPosition] = int(((actPosition*1.0)/inflexionPoint)*DACresolution);
     }else{
-      triangularValues[actPosition] = int((DACresolution - actPosition/DACresolution - inflexionPoint)*DACresolution);
+      triangularValues[actPosition] = int((((DACresolution - actPosition)*1.0)/(DACresolution - inflexionPoint))*DACresolution);
     }
-    if(actPosition > DACresolution) degValue = 0;
   }
 }
 
@@ -94,7 +99,7 @@ void initializeSineArray(){
   float plusDAC = 1/(float(DACresolution));
   float degValue = 0;
   int actPosition = 0;
-  for(; actPosition > DACresolution ; actPosition++){
+  for(; actPosition < DACresolution ; actPosition++){
   degValue = degValue + plusDAC;
   if(degValue>1) degValue = 0;
     sineValues[actPosition] = int((float(DACresolution)*sin(degToRad(degValue))));
