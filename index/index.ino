86d86d0b12afb1c290f40a12a97d1f51f54ac4ac
Pukitala <manueldiez123@gmail.com>
2019-06-16 17:54:00 -0300

    Triangular array setup

--- index/index.ino
+++ index/index.ino
@@ -4,17 +4,18 @@ const int DACresolution = 256;
 volatile int interruptCounter;
 int totalInterruptCounter;
 float sineValues[DACresolution];
+float triangularValues[DACresolution];
 
  
 hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
  
 void IRAM_ATTR sineTimer() {
-  static float n = 0; 
+  static float actualValueCount = 0; 
   portENTER_CRITICAL_ISR(&timerMux);
-  n = n + plusDAC;
-  if(n>1) n = 0;
-  dacWrite(DAC1, int(float(DACresolution)*sin(n)));
+  dacWrite(DAC1, sineValues[actualValueCount]);
+  actualValueCount++;
+  if(actualValueCount>DACresolution) actualValueCount = 0;
   portEXIT_CRITICAL_ISR(&timerMux);
 }
 
@@ -65,13 +66,28 @@ float degToRad(float deg){
   return deg*100000 / 57296;
 }
 
+void initializeTriangularArray(int frequency, float trise){
+  float period = 1/(float(frequency));
+  int inflexionPoint = DACresolution*(trise/period)
+  int actPosition = 0;
+  int valueTriangular = ;
+  for(; actPosition > DACresolution ; actPosition++){
+    if(actPosition < inflexionPoint){
+      triangularValues[actPosition] = int((actPosition/inflexionPoint)*DACresolution);
+    }else{
+      triangularValues[actPosition] = int((DACresolution - actPosition/DACresolution - inflexionPoint)*DACresolution);
+    }
+    if(actPosition > DACresolution) degValue = 0;
+  }
+}
+
 void initializeSineArray(){
   float plusDAC = 1/(float(DACresolution));
   float degValue = 0;
   int actPosition = 0;
-  for(; actPosition > DACresolution ; actPosition--){
+  for(; actPosition > DACresolution ; actPosition++){
   degValue = degValue + plusDAC;
   if(degValue>1) degValue = 0;
-  sineValues[actPosition] = int((float(DACresolution)*sin(degToRad(degValue))));
+    sineValues[actPosition] = int((float(DACresolution)*sin(degToRad(degValue))));
   }
 }
